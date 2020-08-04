820435ab0aea0b604ff228e81ca14b8fff5080b0
junaidansari6311 <junaidansari6311@gmail.com>
2020-08-04 18:26:37 +0530

    ADDED : Validations to user inputs

--- src/com/addressbook/service/IAddressBook.java
+++ src/com/addressbook/service/IAddressBook.java
@@ -1,15 +1,9 @@
 package com.addressbook.service;
 
-import java.io.IOException;
-
 public interface IAddressBook {
     void addPerson();
     void editPerson();
     void deletePerson();
-    void sortByName();
-    void sortByCity();
-    void sortByState();
-    void sortByZip();
     void viewByCityAndState();
     void viewByCityOrState();
 }
